a355cb1fecb1048e03cf94249ddd66abbd1e1bd2
fuby <fuby@y-car.com.cn>
2015-11-07 02:27:26 +0800

    python

---
+++ test.py
@@ -0,0 +1,3 @@
+import nesert
+movies = ["The Holy Grail",1975,"Terry Jones & Terry Gilliam",91,["Graham Chapman",["Michael Palin","Terry Gilliam","Eric Idle","Terry Jones"]]]
+nesert.print_lol(movies,True)
